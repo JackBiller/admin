8f6e26b8334b9dce45c97c530682448a8522f49f
Jack Biller <jackmailteste@gmail.com>
2021-05-27 16:48:24 -0300

    Config constConfigAdd.php

--- controller/constConfig.php
+++ controller/constConfig.php
@@ -4,4 +4,6 @@ define('EXT_VIEW', 'html');
 define('CONFIG_ENV', file_get_contents('../config.env'));
 define('CONFIG_JSON', file_get_contents('../config.json'));
 
+if (is_file(__DIR__ . './constConfigAdd.php')) include __DIR__ . './constConfigAdd.php';
+
 ?>
