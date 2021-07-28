5d2b43292b8b9276ba59db2c6178a76c9c386c61
jackbiller19 <jackbiller19@gmail.com>
2021-07-28 00:37:28 -0300

    Update constConfig.php

--- controller/constConfig.php
+++ controller/constConfig.php
@@ -1,8 +1,8 @@
 <?php
 
 define('EXT_VIEW', 'html');
-define('CONFIG_ENV', file_get_contents('../config.env'));
-define('CONFIG_JSON', file_get_contents('../config.json'));
+define('CONFIG_ENV', file_get_contents(__DIR__ . '/../config.env'));
+define('CONFIG_JSON', file_get_contents(__DIR__ . '/../config.json'));
 
 if (is_file(__DIR__ . './constConfigAdd.php')) include __DIR__ . './constConfigAdd.php';
 
